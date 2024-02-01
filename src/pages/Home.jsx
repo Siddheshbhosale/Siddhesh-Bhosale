1e34cfab9f75e85a282bb20115bd4c25e9bca14b
Siddhesh Bhosale <bhosalesiddhesh9@gmail.com>
2024-02-01 18:19:55 +0530

    photo, responsive.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import './Home.css';
 import Navbar from "./Navbar";
+import photo from '../photo.jpg';
 
 const Home = () => {
     return (
@@ -17,23 +18,23 @@ const Home = () => {
                         <a href="#mega-project-scroll" style={{ textDecoration: "none", color: "inherit" }}>
                             <div className="menu-item">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-folder-open-dot"><path d="m6 14 1.45-2.9A2 2 0 0 1 9.24 10H20a2 2 0 0 1 1.94 2.5l-1.55 6a2 2 0 0 1-1.94 1.5H4a2 2 0 0 1-2-2V5c0-1.1.9-2 2-2h3.93a2 2 0 0 1 1.66.9l.82 1.2a2 2 0 0 0 1.66.9H18a2 2 0 0 1 2 2v2" /><circle cx="14" cy="15" r="1" /></svg>
-                                maga project
+                                Mega project
                             </div>
                         </a>
 
                         <a href="#tech-scroll" style={{ textDecoration: "none", color: "inherit" }}><div className="menu-item">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-scroll"><path d="M8 21h12a2 2 0 0 0 2-2v-2H10v2a2 2 0 1 1-4 0V5a2 2 0 1 0-4 0v3h4" /><path d="M19 17V5a2 2 0 0 0-2-2H4" /></svg>
-                            Technologyies</div>
+                            Technology</div>
                         </a>
 
                         <a href='#mini-project-scroll' style={{ textDecoration: "none", color: "inherit" }}>
                             <div className="menu-item">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-folder-dot"><path d="M4 20h16a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2h-7.93a2 2 0 0 1-1.66-.9l-.82-1.2A2 2 0 0 0 7.93 3H4a2 2 0 0 0-2 2v13c0 1.1.9 2 2 2Z" /><circle cx="12" cy="13" r="1" /></svg>
-                                Min Project
+                                Mini Project
                             </div>
                         </a>
                         
-                        <div style={{ paddingTop: '10px' }}>Profile</div>
+                        <div style={{ paddingTop: '10px' }}>Profiles</div>
                         <div className="menu-item">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#0277b5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect width="4" height="12" x="2" y="9"></rect><circle cx="4" cy="4" r="2"></circle></svg>
                             Linked In
@@ -58,14 +59,17 @@ const Home = () => {
 
                     <section className="profile">
                         <div className="bg-effect">
-                            <div className="profile-image">
-                            </div>
+                            <img className="profile-image" height="120px" width="120px" src={photo} alt="Description" />
                             <div className="profile-info">
                                 <div>
                                     <div className="profile-name">Siddhesh Bhosale</div>
-                                    <div>Profile</div>
+                                    <div style={{display:"flex",justifyContent:"space-between",alignItems:"center",padding:"5px 0"}}>Profile        
+                                        <div className="linkein-icon">
+                                            <a target="_blank"  style={{ width: '24px', height: '24px' }} href="https://www.linkedin.com/in/siddheshbhosale" ><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#0431c7" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect width="4" height="12" x="2" y="9"></rect><circle cx="4" cy="4" r="2"></circle></svg></a>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div className="profile-contact">Linkedin</div>
+                                <div className="profile-contact"><a target="_blank"  style={{ width: '24px', height: '24px', paddingRight:"20px" }} href="https://www.linkedin.com/in/siddheshbhosale" ><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#0431c7" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect width="4" height="12" x="2" y="9"></rect><circle cx="4" cy="4" r="2"></circle></svg></a> Linkedin</div>
                             </div>
                         </div>
                     </section>
@@ -73,7 +77,7 @@ const Home = () => {
 
                     <section className="work-details">
                         <div id="mega-project-scroll" className="mega-project">
-                            <div style={{ fontSize: "1.25rem" }}>Full Stack Projects</div>
+                            <div style={{  fontSize:  "1.25rem"  }}>Full Stack Projects</div>
                             <div className="mega-project-grid">
                                 <a target="_black" href="https://github.com/nwaliaez/discord-clone" style={{ textDecoration: 'none' }}>
                                     <div className="card">
@@ -87,9 +91,9 @@ const Home = () => {
                                 <a target="_black" href="https://github.com/nwaliaez/discord-clone" style={{ textDecoration: 'none' }}>
                                     <div className="card">
                                         <img alt="spotify" src="https://ez-snippet.vercel.app/_next/image?url=%2Fprojects%2Fdiscord-clone.jpeg&w=1920&q=75" />
-                                        <div style={{ display: 'flex', flexDirection: "column", padding: "0.5rem 1rem", height: '6rem' }}>
-                                            <div style={{ display: "flex", fontWeight: "bold" }}>Discord Clone</div>
-                                            <div style={{ display: 'flex', fontSize: "0.75rem" }}>Next.js 13, React Hook Form, Zustand, Shadcn, Typescript</div>
+                                        <div style={{ display: 'flex', flexDirection: "column", padding: "0.5rem 1rem", height:  '6rem'  }}>
+                                            <div style={{  display:  "flex", fontWeight:  "bold"  }}>Discord Clone</div>
+                                            <div style={{  display: 'flex',  fontSize: "0.75rem"  }}>Next.js 13, React Hook Form, Zustand, Shadcn, Typescript</div>
                                         </div>
                                     </div>
                                 </a>
@@ -106,10 +110,10 @@ const Home = () => {
                                             <img width="20px" height="20px" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K" />
                                             React.js
                                         </div>
-                                        <div className="tech-name" > <img width="20px" height="20px" src="https://www.google.com/url?sa=i&url=https%3A%2F%2Ficonscout.com%2Ficons%2Fleetcode&psig=AOvVaw3EacWZU19wRaw-dtMJLw90&ust=1706809662926000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCPih486XiIQDFQAAAAAdAAAAABAE" />JavaScript.js</div>
-                                        <div className="tech-name" style={{ border: '1px solid rgba(234,179,8,.6)' }}>Tailwind CSS</div>
-                                        <div className="tech-name" style={{ border: '1px solid rgba(239,68,68,.6)' }}>HTML</div>
-                                        <div className="tech-name" style={{ border: '1px solid hsla(220,9%,46%,.6)' }}>CSS</div>
+                                        <div className="tech-name" style={{border: '1px solid rgba(59, 130, 246, 0.6)'}}>JavaScript.js</div>
+                                        <div className="tech-name" style={{border: '1px solid rgba(234,179,8,.6)'}}>Tailwind CSS</div>
+                                        <div className="tech-name" style={{border: '1px solid rgba(239,68,68,.6)'}}>HTML</div>
+                                        <div className="tech-name" style={{border: '1px solid hsla(220,9%,46%,.6)'}}>CSS</div>
                                     </div>
                                 </div>
                                 <div className="tech-block">
@@ -129,13 +133,17 @@ const Home = () => {
                             <div id="mini-project-scroll" style={{ fontSize: "1.25rem" }}>Mini Project</div>
                             <div className="mega-project-grid">
                                 <a target="_black" href="https://github.com/nwaliaez/discord-clone" style={{ textDecoration: 'none' }}>
-                                    <div className="card" style={{ backgroundColor: "#1c1b23" }}>
+                                    <div className="card" style={{ backgroundColor: "#1c1b23",gap:"0px" }}>
                                         <img alt="spotify" src="https://ez-snippet.vercel.app/_next/image?url=%2Fprojects%2Fdiscord-clone.jpeg&w=1920&q=75" />
-                                        <div style={{ display: 'flex', flexDirection: "column", padding: "0.5rem 1rem" }}>
+                                        <div class="progress-bar">
+                                            <div style={{backgroundColor:"#2bd576",height:"100%",overflow:"hidden"}}></div>
+                                        </div>
+                                    </div>
+                                        <div style={{ display: 'flex', flexDirection: "column", padding: "0.5rem 1rem",backgroundColor: "#1c1b23" }}>
                                             <div style={{ display: "flex", fontWeight: "bold", color: "white" }}>Discord Clone</div>
                                             <div style={{ display: 'flex', fontSize: "0.75rem", color: "#4f4f52" }}>Next.js 13, React Hook Form, Zustand, Shadcn, Typescript</div>
                                         </div>
-                                    </div>
+                                 
                                 </a>
                                 <a target="_black" href="https://github.com/nwaliaez/discord-clone" style={{ textDecoration: 'none' }}>
                                     <div className="card" style={{ backgroundColor: "#1c1b23" }}>
